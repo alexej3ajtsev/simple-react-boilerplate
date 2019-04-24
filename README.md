065d1931cd9632281f1ae26d32dbc457c79ee6af
zajtsev alexey <zajtsev.lesha2015@yandex.ru>
2019-04-24 10:57:37 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# simple webpack react babel webserver hmr proxy
+## Features
+
+* React 16
+* Webpack 4
+* Babel 7
+* Hot Module Replacement
+* Proxy
+* css
+
+## Installation
+
+* `git clone git@github.com:alexej3ajtsev/simple-react-boilerplate.git your_folder`
+* cd your_folder
+* npm install
+* npm run start
+* visit `http://localhost:8080/`
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  },
+  devServer: {
+    hot: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+    proxy: { 
+      '/api/**': { target: 'http://your.domain-or.ip', secure: false },
+    },
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
